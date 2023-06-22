b2b979770a550426aee5afa9fc18581734bdc515
Laura Cabayol <lauracabayol@gmail.com>
2023-06-22 10:27:04 +0200

    change (completely) the interface for different emulators

--- lace/emulator/emulator.py
+++ lace/emulator/emulator.py
@@ -2,80 +2,69 @@ import sys
 from lace.emulator import gp_emulator
 sys.path.append('emulator.py')
 from lace.emulator import nn_emulator
+from lace.emulator import pnd_archive
+from lace.archive import interface_archive
+
+def P1D_emulator(archive=None, emu_algorithm=None, archive_label = 'Gadget', emulator_label=None, zmax=4.5, kmax_Mpc=4, ndeg=5, train=True, emu_type='polyfit', model_path=None, save_path=None, nepochs_nn=1):
+    
+    
+    """archive: Data archive used for training the emulator. It is an optional argument that must be provided when using a custom emulator.
+    emu_algorithm: This argument specifies the type of emulator algorithm to be used. It is an optional argument that must be provided when using a custom emulator. Options are 'GP' for Gaussian Process and 'NN' for Neural Network.
+    archive_label: Type of archive being used. The default value is set to 'Gadget'. Adding Nyx is work in progress
+    emulator_label: Specific emulator label calling the emulators in Pdersetn et al. 2021: 'Pedersen21', and Cabayol-Garcia et al. 2023:'Cabayol23'. 
+    zmax: This argument specifies the maximum redshift value. It is used when training a custom emulator. The default value is 4.5.
+    kmax_Mpc: This argument represents the maximum wavenumber in units of Mpc^(-1). It is used when training a custom emulator. The default value is 4.
+    ndeg: Degree of the polynomial fit. It is used when training a custom emulator. The default value is 5.
+    train: Boolean flag that determines whether to train the emulator or load a pre-trained one. When set to True, the emulator is trained. When set to False, a pre-trained emulator is loaded. The default value is True. Only valid for the NN emulator. 
+    emu_type: Type of emulator to be used. It is used when training a custom emulator. The default value is 'polyfit'.
+    model_path: String that specifies the path to a pre-trained emulator model.
+    save_path: String that indicates the path to save the emulator model once trained. Only valid for the NN emulator.
+    nepochs_nn: Integer fixing the number of epochs the NN is trained."""
+    
+    
+    if emulator_label=='Pedersen21':
+        print('Select emulator used in Pedersen et al. 2021')
+        emuparams = ['Delta2_p', 'n_p', 'mF', 'sigT_Mpc', 'gamma', 'kF_Mpc']
+        archive = pnd_archive.archivePND(sim_suite="Pedersen21")
+        archive.get_training_data()
+        
+        emulator = GPEmulator(passarchive=archive)
+        return emulator
+
+    elif emulator_label=='Cabayol23':
+        print('Select emulator used in Cabayol-Garcia et al. 2023')
+        emuparams = ['Delta2_p', 'n_p', 'mF', 'sigT_Mpc', 'gamma', 'kF_Mpc']
+        if train==True:
+            print('Train emulator')
+            archive = pnd_archive.archivePND(sim_suite="Cabayol23")
+            archive.get_training_data()
+
+            emulator = NNEmulator(archive,emuparams, nepochs=nepochs_nn, step_size=75, kmax_Mpc=4, ndeg=5, Nsim=30, train=True, emu_type='polyfit')
+        if train==False:
+            if model_path==None:
+                raise ValueError('if train==False, a model path must be provided')
+            else:
+                print('Load pre-trained emulator')
+                archive = pnd_archive.archivePND(sim_suite="Cabayol23")
+                archive.get_training_data()
+                emulator = NNEmulator(archive,emuparams, train=False,model_path=model_path)
+                
+    if emulator_label==None:
+        print('Select custom emulator')
+        if (archive==None)|(emu_algorithm==None):
+            raise ValueError('If an emulator label is not provided, the archive and emulator type are required')
+
+        if (emu_algorithm=='NN')&(archive_label=='Gadget'):
+            emuparams = ['Delta2_p', 'n_p', 'mF', 'sigT_Mpc', 'gamma', 'kF_Mpc']
+            emulator = NNEmulator(archive,emuparams, zmax=zmax, kmax_Mpc=kmax_Mpc, ndeg=ndeg,save_path=save_path )
+
+        if (emu_algorithm=='NN')&(archive_label=='Nyx'):
+            emuparams = ['Delta2_p', 'n_p','mF', 'sigT_Mpc', 'gamma', 'lambda_P']
+            raise Exception('Work in progress')
+
+        if (emu_algorithm=='GP')&(archive_label=='Gadget'):
+            emuparams = ['Delta2_p', 'n_p', 'mF', 'sigT_Mpc', 'gamma', 'kF_Mpc']
+            emulator = GPEmulator(passarchive=archive)
 
 
-class P1Demulator:
-    def __init__(self, emu_algorithm='NN', paramList=['Delta2_p', 'n_p', 'mF', 'sigT_Mpc', 'gamma', 'kF_Mpc'], zmax=4.5, kmax_Mpc=4, ndeg=5, Nsim=30, train=True, emu_type='polyfit', list_archives=['data_input_axes', 'data_input_phases'], postprocessing='768', drop_rescalings=False, drop_sim=None, drop_z=None, passarchive=None, model_path=None, save_path=None):
-        """
-        Initializes the P1Demulator class.
-
-        Args:
-             emu_algorithm (str): The emulator algorithm to use. Possibilities are Gaussian Process 'GP' and Neural Network (NN). Default is 'NN'.
-             paramList (list): List of parameters. Default is ['Delta2_p', 'n_p', 'mF', 'sigT_Mpc', 'gamma', 'kF_Mpc'].
-             zmax (float): Maximum redshift. Default is 4.5.
-             kmax_Mpc (float): Maximum wavenumber in Mpc. Default is 4.
-             ndeg (int): Degree of polynomial for polyfit emulator. Default is 5.
-             Nsim (int): Number of simulations. Default is 30.
-             train (bool): Flag indicating whether to train the emulator. Default is True.
-             emu_type (str): Type of emulator. Possibilities are 'k_bin' and 'polyfit'. Default is 'polyfit'.
-             list_archives (list): List of archives. Default is ['data_input_axes', 'data_input_phases'].
-             postprocessing (str): Post-processing option. Old post-processing along the line of sight: "500". \n
-             New post processing alogn the three axes: "768"Default is '768'.
-             drop_rescalings (bool): Flag indicating whether to drop rescalings. Default is False.\n
-             Setting it to True is recommended only for GP.
-             drop_sim (int): Simulations number from the hypercube to drop from the training sample.
-             drop_z (float): Drop all snapshots at redshift z.
-             passarchive: (class) Archive to train the emulator. Only for the GP.
-             model_path: (str) Path to a pretrained model. Default is None. Only for NN
-             save_path: (str) Path to store the model. Default is None. Only for NN
-        """
-        self.emu_algorithm = emu_algorithm
-        self.paramList = paramList
-
-
-        if postprocessing == '500':
-            # If postprocessing is '500', print a warning message and modify list_archives
-            print('Warning: With the one-axis version of the post-processing, the only archive allowed is "data".\nModified to continue running')
-            list_archives = ['data']
-
-        if (postprocessing != '500')&(postprocessing != '768'):
-            raise ValueError('Available post-processings are "500" and "768"')
-
-
-        if self.emu_algorithm == 'NN':
-            # If emu_algorithm is 'NN', create an instance of NNEmulator and assign it to the emulator attribute
-            self.emulator = nn_emulator.NNEmulator(
-                 paramList=paramList,
-                 kmax_Mpc=4,
-                 zmax=4.5,
-                 ndeg=5,
-                 nepochs=100,
-                 step_size=75,
-                 train=True,
-                 postprocessing='768',
-                 model_path=model_path,
-                 Nsim=30,
-                 list_archives=list_archives,
-                 drop_sim=None,
-                 drop_z=None,
-                 pick_z=None,
-                 save_path=save_path,
-                 drop_rescalings=drop_rescalings
-             )
-
-
-        if self.emu_algorithm == 'GP':
-            # If emu_algorithm is 'GP', create an instance of GPEmulator and assign it to the emulator attribute
-            self.emulator = gp_emulator.GPEmulator(
-                 z_max=zmax,
-                 kmax_Mpc=kmax_Mpc,
-                 ndeg=ndeg,
-                 train=True,
-                 asymmetric_kernel=True,
-                 rbf_only=True,
-                 passarchive=None,
-                 emu_type=emu_type,
-                 check_hull=False,
-                 key_data=list_archives[0]
-            ) 
-
+    return emulator
