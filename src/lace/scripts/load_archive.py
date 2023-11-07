a0f796624f58e122c2c2f3d163ad5ccd6d2c59a5
Jonás Chaves-Montero <35238605+jchavesmontero@users.noreply.github.com>
2023-11-07 17:01:07 +0100

    Nyx cosmo (#62)
    * multiple updates
    
    * added default value for nyx_version
    
    * update readme file
    
    * add slash to script path
    
    * nothing
    
    * Get the nyx_cosmo branch up to data (#61)
    
    * Include nyx_version flag in bookkeeper (#54)
    
    * multiple updates
    
    * added default value for nyx_version
    
    * update readme file
    
    * add slash to script path
    
    * scripts to test the emulators and archives
    
    * Extended emulator weighting (#57)
    
    * add weighting function for the extended emulator
    
    * update a weighting option for the extended emulator
    
    * fix import of polyfit + include extended emulator example
    
    * include default option for emulator and option of downweighting small scales
    
    * change emulator label of the Nyx NN emualtor
    
    * change the nyx emulator label
    
    ---------
    
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login23.chn>
    
    * Remove randomness (#59)
    
    * add lines to control the randomness of the NN emulator and ensure reproducibility
    
    * add random package to the list of requirements
    
    * add prints of number of training points and time for consistensy with the GP emulator
    
    * change typo in print
    
    ---------
    
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login23.chn>
    
    * drop random from requirements (#60)
    
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login23.chn>
    
    ---------
    
    Co-authored-by: Andreu Font-Ribera <andreufont@users.noreply.github.com>
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login23.chn>
    Co-authored-by: Laura <lauracabayol@gmail.com>
    
    * using kf for nyx
    
    * check notebooks
    
    * fixing notebook
    
    * Merge optimize nyx with nyx_cosmo (#64)
    
    * drop random from requirements
    
    * add nhidden as a class argument
    
    * fix error with loading trained Nyx models
    
    * script to train and save models
    
    * fix typo saving name
    
    * add Nyx_v1 to config
    
    * update lace models
    
    * new Nyx model
    
    ---------
    
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login23.chn>
    
    * automatic script for l1O tests
    
    * load_archive
    
    * l1O automatic
    
    * drop load of initial weights. This is no longer needed since we are now fixing the initial seed
    
    * delete initial weights
    
    * minor change load initial weights
    
    * add Nyx_v1_extended option
    
    * delete duplicated line
    
    * store emus
    
    * updating notebooks
    
    * option to read pretrained emulators without reading again the archive
    
    ---------
    
    Co-authored-by: Andreu Font-Ribera <andreufont@users.noreply.github.com>
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login23.chn>
    Co-authored-by: Laura <lauracabayol@gmail.com>
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login03.chn>

--- src/lace/scripts/load_archive.py
+++ src/lace/scripts/load_archive.py
@@ -1,15 +1,12 @@
-import numpy as np
-import sys
-import os
-import json
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-
-from lace.emulator import p1d_archive
-basedir='lace/emulator/sim_suites/Australia20/'
-drop_tau_rescalings=True
-archive=p1d_archive.archiveP1D(basedir=basedir,
-                    drop_tau_rescalings=drop_tau_rescalings,verbose=True)
+from lace.archive.gadget_archive import GadgetArchive
 
 
+def main():
+    print("Testing loading archive")
+    cabayol23_archive = GadgetArchive(postproc="Cabayol23")
+    print(cabayol23_archive.list_sim)
+    print("Done!")
 
+
+if __name__ == "__main__":
+    main()
