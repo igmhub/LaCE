a9467a47c8ef8568c63e4b07d49777b7e41b6c17
Laura Cabayol-Garcia <lcabayol@login38.chn>
2023-11-09 03:44:20 -0800

    drop nyx_v1 as option

--- src/lace/emulator/nn_emulator.py
+++ src/lace/emulator/nn_emulator.py
@@ -105,9 +105,7 @@ class NNEmulator(base_emulator.BaseEmulator):
         emulator_label_all = [
             "Cabayol23",
             "Nyx_v0",
-            "Nyx_v1",
             "Cabayol23_extended",
-            "Nyx_v1_extended",
         ]
         if emulator_label is not None:
             try:
@@ -185,29 +183,6 @@ class NNEmulator(base_emulator.BaseEmulator):
                 self.nhidden,
             ) = (4, 5, 1000, 750, 5)
 
-        elif emulator_label == "Nyx_v1":
-            print(
-                r"Neural network emulating the optimal P1D of Nyx simulations "
-                + "fitting coefficients to a 5th degree polynomial. It "
-                + "goes to scales of 4Mpc^{-1} and z<=4.5. The parameters "
-                + "passed to the emulator will be overwritten to match "
-                + "these ones"
-            )
-            self.emu_params = [
-                "Delta2_p",
-                "n_p",
-                "mF",
-                "sigT_Mpc",
-                "gamma",
-                "kF_Mpc",
-            ]
-            (
-                self.kmax_Mpc,
-                self.ndeg,
-                self.nepochs,
-                self.step_size,
-                self.nhidden,
-            ) = (4, 5, 1000, 750, 2)
 
         elif emulator_label == "Cabayol23_extended":
             print(
@@ -235,7 +210,7 @@ class NNEmulator(base_emulator.BaseEmulator):
                 self.weighted_emulator,
             ) = (8, 7, 100, 75, 5, False)
 
-        elif emulator_label == "Nyx_v1_extended":
+        elif emulator_label == "Nyx_v0_extended":
             print(
                 r"Neural network emulating the optimal P1D of Nyx simulations "
                 + "fitting coefficients to a 5th degree polynomial. It "
@@ -258,7 +233,7 @@ class NNEmulator(base_emulator.BaseEmulator):
                 self.step_size,
                 self.nhidden,
                 self.weighted_emulator,
-            ) = (8, 7, 1000, 750, 2, True)
+            ) = (8, 7, 1000, 750, 5, True)
 
         # set archive and training set
         if (archive is not None) and (training_set is None):
@@ -299,7 +274,7 @@ class NNEmulator(base_emulator.BaseEmulator):
             if "mpg_" not in self.training_data[0]["sim_label"]:
                 raise ValueError("Training data are not Gadget sims")
 
-        if (emulator_label == "Nyx_v0") | (emulator_label == "Nyx_v1"):
+        if (emulator_label == "Nyx_v0"):
             # make sure that input archive / training data are Nyx sims
             if "nyx_" not in self.training_data[0]["sim_label"]:
                 raise ValueError("Training data are not Nyx sims")
