9080ff61fdfe4750b09ee4e078c01c197e25c2b7
Jonás Chaves-Montero <35238605+jchavesmontero@users.noreply.github.com>
2024-02-28 10:51:15 +0100

    Add emulator manager (#83)
    * update with new cabayol23
    
    * Update models (#80) (#81)
    
    * change trainng_set in metadata
    
    * load full emulator
    
    ---------
    
    Co-authored-by: Laura <lauracabayol@gmail.com>
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login31.chn.perlmutter.nersc.gov>
    
    * update notebook for new NN emulator
    
    * Adding emulator manager
    
    * emulator manager
    
    * nonlinear smoothing class
    
    * smoothing manager
    
    * a
    
    * add scikit dependency
    
    * prints in emu manager
    
    ---------
    
    Co-authored-by: Laura <lauracabayol@gmail.com>
    Co-authored-by: Laura Cabayol-Garcia <lcabayol@login31.chn.perlmutter.nersc.gov>

---
+++ lace/utils/smoothing_manager.py
@@ -0,0 +1,42 @@
+from lace.utils import poly_p1d
+
+
+def apply_smoothing(emulator, data, fprint=print):
+    """Apply smoothing to the p1d"""
+
+    type_data = type(data)
+    if type_data is not list:
+        data = [data]
+
+    if (data[0]["p1d_Mpc"] is None) | (data[0]["k_Mpc"] is None):
+        raise ValueError("p1d_Mpc or k_Mpc not defined")
+
+    if emulator.emu_type == "k_bin":
+        # No smoothing
+        fprint("k-bin emulator, no smoothing is applied")
+        pass
+    elif emulator.emu_type == "polyfit":
+        fprint("polyfit emulator, smoothing is applied")
+        # Polynomial smoothing
+        for ii in range(len(data)):
+            fit_p1d = poly_p1d.PolyP1D(
+                data[ii]["k_Mpc"],
+                data[ii]["p1d_Mpc"],
+                kmin_Mpc=1e-10,
+                kmax_Mpc=emulator.kmax_Mpc,
+                deg=emulator.ndeg,
+            )
+            data[ii]["p1d_Mpc_smooth"] = fit_p1d.P_Mpc(data[ii]["k_Mpc"])
+    elif emulator.emu_type == "k_bin_sm":
+        fprint("k_bin_sm emulator, smoothing is applied")
+        # Nonlinear smoothing
+        for ii in range(len(data)):
+            _ = emulator.Kernel_Smoothing.apply_kernel_smoothing(
+                data[ii]["k_Mpc"], data[ii]
+            )
+            data[ii]["p1d_Mpc_smooth"] = _
+    else:
+        raise ValueError("Smoothing technique not recognized:", technique)
+
+    if type_data is not list:
+        data = data[0]
