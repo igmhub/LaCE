67d7727dd0b25d364165539d0e0e69a781c0ee36
jchavesmontero <jchaves@ifae.es>
2024-05-28 11:33:36 +0200

    update emulator_manager

--- lace/emulator/emulator_manager.py
+++ lace/emulator/emulator_manager.py
@@ -5,17 +5,46 @@ from lace.emulator.gp_emulator import GPEmulator
 
 
 def emulators_supported():
-    """List of emulators supported"""
+    """List of emulators supported
+    LaCE emulators:
+        Pedersen21: GPEmulator used in Pedersen21, k-bin emulator.
+            Superseed by Pedersen21_ext
+        Pedersen21_ext: GPEmulator like Pedersen21 but using Cabayol23 postprocessing.
+        Pedersen21_ext8: GPEmulator like Pedersen21_ext but accessing smaller scales.
+        Pedersen23: GPEmulator used in Pedersen23, polynomial emulator.
+            Superseed by Pedersen23_ext
+        Pedersen23_ext: GPEmulator like Pedersen23 but using Cabayol23 postprocessing.
+            Recommended GP emulator
+        Pedersen23_ext8: GPEmulator like Pedersen23_ext but accessing smaller scales
+            Recommended GP emulator for accessing small scales
+        CH24: GPEmulator based on non-linear smoothing
+        Cabayol23: NNEmulator used in Cabayol23, polynomial emulator.
+            Superseed by Cabayol23+
+        Cabayol23+: NNEmulator like Cabayol23 but using better architecture
+            Recommended NN emulator
+        Cabayol23_extended: NNEmulator used in Cabayol23 accessing smaller scales than Cabayol23
+            Superseed by Cabayol23_extended+
+        Cabayol23+_extended: NNEmulator like Cabayol23_extended but using better architecture
+            Recommended NN emulator for accessing small scales
+
+    Nyx emulators:
+        Nyx_v0: NNEmulator using amplitude and slope, polynomial emulator.
+            Superseed by Nyx_alphap
+        Nyx_alphap: NNEmulator using amplitude, slope, and running, polynomial emulator.
+            Recommended emulator
+
+    """
 
     emulators_supported = [
         "Pedersen21",
+        "Pedersen21_ext", 
         "Pedersen23",
-        "Pedersen21_ext",
         "Pedersen23_ext",
         "CH24",
         "Cabayol23",
-        "Cabayol23+",
         "Cabayol23_extended",
+        "Cabayol23+",
+        "Cabayol23+_extended",
         "Pedersen21_ext8",
         "Pedersen23_ext8",
         "Nyx_v0",
@@ -98,6 +127,7 @@ def set_emulator(emulator_label, archive=None, drop_sim=None):
         (emulator_label == "Cabayol23")
         | (emulator_label == "Cabayol23+")
         | (emulator_label == "Cabayol23_extended")
+        | (emulator_label == "Cabayol23+_extended")
     ):
         if (emulator_label == "Cabayol23") | (
             emulator_label == "Cabayol23_extended"
@@ -105,6 +135,8 @@ def set_emulator(emulator_label, archive=None, drop_sim=None):
             folder = "NNmodels/Cabayol23_Feb2024/"
         elif emulator_label == "Cabayol23+":
             folder = "NNmodels/Cabayol23+/"
+        elif emulator_label == "Cabayol23+_extended":
+            folder = "NNmodels/Cabayol23+_extended/"
 
         if drop_sim is None:
             model_path = folder + emulator_label + ".pt"
