9b92fae41dfd8416f8a4bcd4134f40fa6b9cf6cd
Laura Cabayol-Garcia <lcabayol@login12.chn.perlmutter.nersc.gov>
2024-08-05 00:32:37 -0700

    add extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,12 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
