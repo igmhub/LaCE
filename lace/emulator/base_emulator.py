6b7159e3afe77c7ee1ad857d6ba62c8d61bd7642
Jonás Chaves-Montero <35238605+jchavesmontero@users.noreply.github.com>
2024-08-27 15:53:23 +0200

    speed nn (#104)
    * speed nn
    
    * speed up gp emu

--- lace/emulator/base_emulator.py
+++ lace/emulator/base_emulator.py
@@ -1,11 +1,22 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
+
 class BaseEmulator(ABC):
     """Minimum amount of documentation"""
- 
-    @abstractmethod
-    def emulate_p1d_Mpc(self,model,k_Mpc,return_covar=False,z=None):
-        """Minimum amount of documentation (input, output) """
-        pass
 
+    @abstractmethod
+    def emulate_p1d_Mpc(self, model, k_Mpc, return_covar=False, z=None):
+        """
+        Emulate P1D values for a given set of k values in Mpc units.
+
+        Args:
+            model (dict): Dictionary containing the model parameters.
+            k_Mpc (np.ndarray): Array of k values in Mpc units.
+            return_covar (bool, optional): Whether to return covariance. Defaults to False.
+            z (float, optional): Redshift value. Defaults to None.
+
+        Returns:
+            np.ndarray: Emulated P1D values.
+        """
+        pass
