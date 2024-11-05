b5e52aee071b934d7c2ea59759a9095830f9b9da
jchavesmontero <jchaves@ifae.es>
2024-11-05 15:32:59 +0100

    update IGM files mpg sims

--- scripts/save_mpg_IGM.py
+++ scripts/save_mpg_IGM.py
@@ -22,60 +22,90 @@ def main():
 
     ind_axis = "average"
     ind_phase = "average"
-    val_scaling = 1
 
     list_snap = np.unique(cabayol23_archive.ind_snap)
     nz = len(list_snap)
     dict_index = {}
 
     for sim_label in cabayol23_archive.list_sim_cube:
-        cosmo_params, linP_params = cabayol23_archive._get_emu_cosmo(sim_label)
-        dict_index[sim_label] = {}
-        dict_index[sim_label]["z"] = np.zeros(nz)
-        dict_index[sim_label]["tau_eff"] = np.zeros(nz)
-        dict_index[sim_label]["gamma"] = np.zeros(nz)
-        dict_index[sim_label]["sigT_kms"] = np.zeros(nz)
-        dict_index[sim_label]["kF_kms"] = np.zeros(nz)
-        for ind_snap in list_snap:
-            for ind_book in range(len(training)):
-                if (
-                    (training[ind_book]["ind_snap"] == ind_snap)
-                    & (training[ind_book]["ind_axis"] == ind_axis)
-                    & (training[ind_book]["ind_phase"] == ind_phase)
-                    & (training[ind_book]["sim_label"] == sim_label)
-                    & (training[ind_book]["val_scaling"] == val_scaling)
-                ):
-                    dict_index[sim_label]["z"][ind_snap] = training[ind_book][
-                        "z"
-                    ]
-                    dict_index[sim_label]["tau_eff"][ind_snap] = -np.log(
-                        training[ind_book]["mF"]
-                    )
-                    dict_index[sim_label]["gamma"][ind_snap] = training[
-                        ind_book
-                    ]["gamma"]
-                    _ = thermal_broadening_kms(training[ind_book]["T0"])
-                    dict_index[sim_label]["sigT_kms"][ind_snap] = _
+        (
+            cosmo_params,
+            linP_params,
+            star_params,
+        ) = cabayol23_archive._get_emu_cosmo(sim_label)
+        for ind_rescaling in range(5):
+            lab = sim_label + "_" + str(ind_rescaling)
+            dict_index[lab] = {}
+            dict_index[lab]["z"] = np.zeros(nz)
+            dict_index[lab]["tau_eff"] = np.zeros(nz)
+            dict_index[lab]["mF"] = np.zeros(nz)
+            dict_index[lab]["gamma"] = np.zeros(nz)
+            dict_index[lab]["sigT_kms"] = np.zeros(nz)
+            dict_index[lab]["sigT_Mpc"] = np.zeros(nz)
+            dict_index[lab]["kF_kms"] = np.zeros(nz)
+            dict_index[lab]["kF_Mpc"] = np.zeros(nz)
+            dict_index[lab]["val_scaling"] = np.zeros(nz)
 
-                    ind_z = np.argwhere(
-                        training[ind_book]["z"] == linP_params["z"]
-                    )[0, 0]
-                    _ = (
-                        training[ind_book]["kF_Mpc"]
-                        / linP_params["dkms_dMpc"][ind_z]
-                    )
-                    dict_index[sim_label]["kF_kms"][ind_snap] = _
+            for ind_snap in list_snap:
+                for ind_book in range(len(training)):
+                    if (
+                        (training[ind_book]["ind_snap"] == ind_snap)
+                        & (training[ind_book]["ind_axis"] == ind_axis)
+                        & (training[ind_book]["ind_phase"] == ind_phase)
+                        & (training[ind_book]["sim_label"] == sim_label)
+                        & (training[ind_book]["ind_rescaling"] == ind_rescaling)
+                    ):
+                        dict_index[lab]["z"][ind_snap] = training[ind_book]["z"]
+                        dict_index[lab]["val_scaling"][ind_snap] = training[
+                            ind_book
+                        ]["val_scaling"]
+                        dict_index[lab]["mF"][ind_snap] = training[ind_book][
+                            "mF"
+                        ]
+                        dict_index[lab]["sigT_Mpc"][ind_snap] = training[
+                            ind_book
+                        ]["sigT_Mpc"]
+                        dict_index[lab]["kF_Mpc"][ind_snap] = training[
+                            ind_book
+                        ]["kF_Mpc"]
+                        dict_index[lab]["tau_eff"][ind_snap] = -np.log(
+                            training[ind_book]["mF"]
+                        )
+                        dict_index[lab]["gamma"][ind_snap] = training[ind_book][
+                            "gamma"
+                        ]
+                        _ = thermal_broadening_kms(training[ind_book]["T0"])
+                        dict_index[lab]["sigT_kms"][ind_snap] = _
 
+                        ind_z = np.argwhere(
+                            training[ind_book]["z"] == linP_params["z"]
+                        )[0, 0]
+                        _ = (
+                            training[ind_book]["kF_Mpc"]
+                            / linP_params["dkms_dMpc"][ind_z]
+                        )
+                        dict_index[lab]["kF_kms"][ind_snap] = _
+
+    ind_axis = "average"
+    ind_phase = "average"
+    val_scaling = 1
     # testing
-
     for sim_label in cabayol23_archive.list_sim_test:
-        cosmo_params, linP_params = cabayol23_archive._get_emu_cosmo(sim_label)
+        (
+            cosmo_params,
+            linP_params,
+            star_params,
+        ) = cabayol23_archive._get_emu_cosmo(sim_label)
         dict_index[sim_label] = {}
         dict_index[sim_label]["z"] = np.zeros(nz)
+        dict_index[sim_label]["val_scaling"] = np.zeros(nz)
         dict_index[sim_label]["tau_eff"] = np.zeros(nz)
+        dict_index[sim_label]["mF"] = np.zeros(nz)
         dict_index[sim_label]["gamma"] = np.zeros(nz)
         dict_index[sim_label]["sigT_kms"] = np.zeros(nz)
+        dict_index[sim_label]["sigT_Mpc"] = np.zeros(nz)
         dict_index[sim_label]["kF_kms"] = np.zeros(nz)
+        dict_index[sim_label]["kF_Mpc"] = np.zeros(nz)
         testing = cabayol23_archive.get_testing_data(sim_label)
         for ind_snap in list_snap:
             for ind_book in range(len(testing)):
@@ -89,6 +119,18 @@ def main():
                     dict_index[sim_label]["z"][ind_snap] = testing[ind_book][
                         "z"
                     ]
+                    dict_index[sim_label]["val_scaling"][ind_snap] = testing[
+                        ind_book
+                    ]["val_scaling"]
+                    dict_index[sim_label]["mF"][ind_snap] = testing[ind_book][
+                        "mF"
+                    ]
+                    dict_index[sim_label]["sigT_Mpc"][ind_snap] = testing[
+                        ind_book
+                    ]["sigT_Mpc"]
+                    dict_index[sim_label]["kF_Mpc"][ind_snap] = testing[
+                        ind_book
+                    ]["kF_Mpc"]
                     dict_index[sim_label]["tau_eff"][ind_snap] = -np.log(
                         testing[ind_book]["mF"]
                     )
@@ -107,9 +149,9 @@ def main():
                     )
                     dict_index[sim_label]["kF_kms"][ind_snap] = _
 
-    folder = os.environ["LACE_REPO"] + "/src/lace/data/sim_suites/Australia20/"
+    folder = os.environ["LACE_REPO"] + "/data/sim_suites/Australia20/"
     np.save(folder + "IGM_histories.npy", dict_index)
-    folder = os.environ["LACE_REPO"] + "/src/lace/data/sim_suites/post_768/"
+    folder = os.environ["LACE_REPO"] + "/data/sim_suites/post_768/"
     np.save(folder + "IGM_histories.npy", dict_index)
 
 
