f73aa1ded98bb1994f91720d691918b4108f5160
Laura Cabayol Garcia <lauracabayol@Lauras-MacBook-Pro.local>
2024-12-13 19:51:29 -0500

    reformat loop

--- lace/utils/exceptions.py
+++ lace/utils/exceptions.py
@@ -1,10 +1,8 @@
 class ExceptionList(Exception):
     def __init__(self, msg, values):
         self.message = msg
-        for ii, val in enumerate(values):
-            self.message += " " + val
-            if ii < len(values) - 1:
-                self.message += ","
+        values = [str(val) for val in values]  
+        self.message += " " + ", ".join(values)
 
     def __str__(self):
         return self.message
