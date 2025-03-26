7b5e1b8cd81ede644099ff0ed08c80672685074e
jchavesmontero <jchaves@ifae.es>
2025-03-26 11:38:55 +0100

    function to train GP emulators

---
+++ scripts/train_GPs.py
@@ -0,0 +1,26 @@
+import numpy as np
+from lace.emulator.emulator_manager import set_emulator
+from lace.archive import gadget_archive, nyx_archive
+from lace.emulator.gp_emulator_new import GPEmulator
+
+
+def main():
+    """Train and store latest GP models. One need to be done once"""
+
+    # Train CH24_mpg_gp emulator
+    archive = gadget_archive.GadgetArchive(postproc="Cabayol23")
+    print("Training CH24_mpg_gp emulator")
+    emulator = GPEmulator(
+        archive=archive, emulator_label="CH24_mpg_gp", train=True
+    )
+
+    # Train CH24_nyx_gp emulator
+    archive = nyx_archive.NyxArchive(nyx_version="Nyx23_Jul2024")
+    print("Training CH24_nyx_gp emulator")
+    emulator = GPEmulator(
+        archive=archive, emulator_label="CH24_nyx_gp", train=True
+    )
+
+
+if __name__ == "__main__":
+    main()
