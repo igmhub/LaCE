013c848f17ed9322a2d3bda31169d53163657378
jchavesmontero <jchaves@ifae.es>
2026-02-09 23:55:59 +0100

    update notebook

--- notebooks/compute_covariance.py
+++ notebooks/compute_covariance.py
@@ -13,7 +13,7 @@
 #     name: python3
 # ---
 
-# # Compute covariance matrix, also figure smoothing
+# # Compute covariance matrix
 
 # First show how to train and load emulators, then compute covariance matrix
 
@@ -38,8 +38,6 @@ archive = gadget_archive.GadgetArchive(postproc="Cabayol23")
 
 archive = nyx_archive.NyxArchive(nyx_version="models_Nyx_Sept2025_include_Nyx_fid_rseed")
 
-# ## Train GPs and check precision
-
 # +
 # train = True
 # # train = True
@@ -48,7 +46,7 @@ archive = nyx_archive.NyxArchive(nyx_version="models_Nyx_Sept2025_include_Nyx_fi
 # emulator_keep = GPEmulator(emulator_label=emulator_label, archive=archive, archive2=archive2, train=train, drop_sim=None)
 # -
 
-# Full
+# # Full
 
 train = True
 # train = False
@@ -59,184 +57,22 @@ emulator = GPEmulator(emulator_label=emulator_label, archive=archive, train=trai
 
 # +
 
-# emulator_label = "CH24_mpgcen_gpr"
-emulator_label = "CH24_nyxcen_gpr"
+emulator_label = "CH24_mpgcen_gpr"
+# emulator_label = "CH24_nyxcen_gpr"
 
 emulator = GPEmulator(
     emulator_label=emulator_label, 
     train=False,
 )
-# emulator = GPEmulator(
-#     emulator_label=emulator_label, 
-#     train=False, 
-#     drop_sim="nyx_0"
-# )
 # -
 
-# Validate central!
-
-# +
-
-# testing_data = archive.get_testing_data("mpg_central")
-# testing_data = archive.get_testing_data("mpg_seed")
-
-testing_data = archive.get_testing_data("nyx_central")
-# testing_data = archive.get_testing_data("nyx_seed")
-
-# testing_data = archive.get_testing_data("nyx_0")
-# emulator = GPEmulator(emulator_label=emulator_label, train=False, drop_sim=isim)
-
-_k_Mpc = testing_data[0]['k_Mpc']
-ind = (_k_Mpc < emulator.kmax_Mpc) & (_k_Mpc > 0)
-k_Mpc = _k_Mpc[ind]
-k_fit = k_Mpc/emulator.kmax_Mpc
-jj = 0
-if jj == 0:
-    k_Mpc_0 = k_Mpc.copy()
-else:
-    if np.allclose(k_Mpc_0, k_Mpc) == False:
-        print(jj, k_Mpc)
-        
-
-nz = len(testing_data)
-
-p1d_Mpc_sim = np.zeros((nz, k_Mpc.shape[0]))
-p1d_Mpc_emu = np.zeros((nz, k_Mpc.shape[0]))
-p1d_Mpc_sm = np.zeros((nz, k_Mpc.shape[0]))
-zz_full = np.zeros((nz, k_Mpc.shape[0]))
-k_Mpc_full = np.zeros((nz, k_Mpc.shape[0]))
-
-for ii in range(nz):
-    # if ("kF_Mpc" not in testing_data[ii]) | (np.isfinite(testing_data[ii]["kF_Mpc"]) == False):
-        
-    #     continue
-
-    # i2 = np.argwhere(np.abs(zz - testing_data[ii]["z"]) < 0.05)[:, 0]
-    # if len(i2) == 0:
-        # continue
-    # else:
-        # i2 = i2[0]
-    # print(jj, ii, testing_data[ii]["z"], i2)
-
-    zz_full[ii] = testing_data[ii]["z"]
-    k_Mpc_full[ii] = k_Mpc_0
-
-    p1d_Mpc_sim[ii] = testing_data[ii]['p1d_Mpc'][ind]
-    # p1d_Mpc_emu[ii] = emulator.emulate_p1d_Mpc(
-    #     testing_data[ii], 
-    #     k_Mpc
-    # )
-    norm = np.interp(
-        k_Mpc, emulator.input_norm["k_Mpc"], emulator.norm_imF(testing_data[ii]["mF"])
-    )
-    yfit = np.log(testing_data[ii]["p1d_Mpc"][ind]/norm)
-    popt, _ = curve_fit(emulator.func_poly, k_fit, yfit)
-    p1d_Mpc_sm[ii] = norm * np.exp(emulator.func_poly(k_fit, *popt))
-# -
-
-zz_full_seed = zz_full.copy()
-p1d_Mpc_sim_seed = p1d_Mpc_sim.copy()
-p1d_Mpc_sm_seed = p1d_Mpc_sm.copy()
-
-# +
-
-from matplotlib import rcParams
-
-rcParams["mathtext.fontset"] = "stix"
-rcParams["font.family"] = "STIXGeneral"
-
-
-# +
-store_data = {}
-
-fig, ax = plt.subplots(1, figsize=(8, 6))
-ftsize = 24
-
-ztar = 3
-ii = np.argwhere(zz_full == ztar)[0,0]
-jj = np.argwhere(zz_full_seed == ztar)[0,0] 
-    
-psm = 0.5 * (p1d_Mpc_sm_seed[jj] + p1d_Mpc_sm[ii])
-# ax.plot(k_Mpc_full[ii], p1d_Mpc_sm[ii]/p1d_Mpc_sim[ii]-1, "C0-", lw=2, label="mpg-central")
-# ax.plot(k_Mpc_full[ii], p1d_Mpc_sim_seed[ii]/p1d_Mpc_sm_seed[ii]-1, "C1--", lw=2, label="mpg-seed")
-ax.plot(k_Mpc_full[ii], p1d_Mpc_sim[ii]/psm-1, "C0-", lw=2, label=r"$P_\mathrm{1D}^x=P_\mathrm{1D}^\mathrm{central}$")
-ax.plot(k_Mpc_full[ii], p1d_Mpc_sim_seed[jj]/psm-1, "C1-", lw=2, label=r"$P_\mathrm{1D}^x=P_\mathrm{1D}^\mathrm{seed}$")
-ax.plot(k_Mpc_full[ii], p1d_Mpc_sm[ii]/psm-1, "C0--", lw=2, label=r"$P_\mathrm{1D}^x=P_\mathrm{1D}^\mathrm{sm,\,central}$")
-ax.plot(k_Mpc_full[ii], p1d_Mpc_sm_seed[jj]/psm-1, "C1--", lw=2, label=r"$P_\mathrm{1D}^x=P_\mathrm{1D}^\mathrm{sm,\,seed}$")
-
-store_data["x"] = k_Mpc_full[ii]
-store_data["y_blue_solid"] = p1d_Mpc_sim[ii]/psm-1
-store_data["y_orange_solid"] = p1d_Mpc_sim_seed[ii]/psm-1
-store_data["y_blue_dashed"] = p1d_Mpc_sm[ii]/psm-1
-store_data["y_orange_dashed"] = p1d_Mpc_sm_seed[ii]/psm-1
-
-
-ax.axhline(color="k", linestyle=":")
-ax.axhline(0.01, color="k", linestyle="--")
-ax.axhline(-0.01, color="k", linestyle="--")
-
-
-# ax.set_ylim(-0.022, 0.028)
-ax.set_ylim(-0.06, 0.1)
-
-
-ax.set_xscale("log")
-ax.set_ylabel(r"$P_\mathrm{1D}^x/P_\mathrm{1D}^\mathrm{sm, average}-1$", fontsize=ftsize)
-ax.set_xlabel(r"$k_\parallel\,\left[\mathrm{Mpc}^{-1}\right]$", fontsize=ftsize)
-ax.tick_params(axis="both", which="major", labelsize=ftsize)
-
-ax.xaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-plt.legend(fontsize=ftsize-2, loc="upper right", ncol=2)
-plt.tight_layout()
-# plt.savefig("figs/smooth_cen_seed.png")
-# plt.savefig("figs/smooth_cen_seed.pdf")
-# plt.savefig("figs/smooth_cen_seed_nyx.png")
-# plt.savefig("figs/smooth_cen_seed_nyx.pdf")
-# +
-import cup1d, os
-
-path_out = os.path.join(os.path.dirname(cup1d.__path__[0]), "data", "zenodo")
-fname = os.path.join(path_out, "fig_3a.npy")
-np.save(fname, store_data)
-
-# +
-import cup1d, os
-
-path_out = os.path.join(os.path.dirname(cup1d.__path__[0]), "data", "zenodo")
-fname = os.path.join(path_out, "fig_B1a.npy")
-np.save(fname, store_data)
-# -
-
-
-
-p1d_Mpc_emu1 = p1d_Mpc_emu.copy()
-
-for ii in range(nz):
-    plt.plot(k_Mpc_full[ii], p1d_Mpc_emu1[ii]/p1d_Mpc_emu[ii], label=zz_full[ii,0 ])
-plt.legend()
-
-for ii in range(nz):
-    plt.plot(k_Mpc_full[ii], p1d_Mpc_emu[ii]/p1d_Mpc_sm[ii], label=zz_full[ii,0 ])
-plt.legend()
-
-# +
-# train = False
-# # train = True
-# # emulator_label = "CH24_mpg_gp"
-# emulator_label = "CH24_nyx_gp"
-# emulator = GPEmulator(emulator_label=emulator_label, archive=archive, train=train, drop_sim=None)
-# -
-
-# L10
+# ## L10
 
 # +
 train = True
 
-# emulator_label = "CH24_mpg_gp"
-# emulator_label = "CH24_nyx_gpr"
-emulator_label = "CH24_nyxcen_gpr"
 # emulator_label = "CH24_mpgcen_gpr"
-# emulator_label = "CH24_mpg_gpr"
+emulator_label = "CH24_nyxcen_gpr"
 if train:
     for ii, isim in enumerate(archive.list_sim_cube):
         if (ii >= 14) & ("nyx" in emulator_label):
